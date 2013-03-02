6dfe372015fa8acfdfe111de3a839d8eea4e3933
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-03-02 11:32:16 -0500

    Added new brew methods and tweaked settings

--- BrewTweaks/BrewMethodDataController.m
+++ BrewTweaks/BrewMethodDataController.m
@@ -18,23 +18,31 @@
     NSMutableArray *methodList = [[NSMutableArray alloc] init];
     NSMutableArray *grindTypes = [[NSMutableArray alloc] init];
     grindTypes[0] = @"Coarse";
-    grindTypes[1] = @"Medium Coarse";
+    grindTypes[1] = @"Medium-Coarse";
     grindTypes[2] = @"Medium";
-    grindTypes[3] = @"Fine";    
+    grindTypes[3] = @"Medium-Fine";
+    grindTypes[4] = @"Fine";
     
     self.masterBrewMethodList = methodList;
     BrewMethod *method;
     
-    method = [[BrewMethod alloc] initWithName:@"French Press" grind:grindTypes[0] coffeeAmount:28.0 waterAmount:392.0 temp:200.0];
+    method = [[BrewMethod alloc] initWithName:@"French Press" grind:grindTypes[0] coffeeAmount:25.0 waterAmount:400.0 temp:195.0];
 
     [self addBrewMethodWithBrewMethod:method];
     
-    method = [[BrewMethod alloc] initWithName:@"Chemex" grind:grindTypes[2] coffeeAmount:28.0 waterAmount:490.0 temp:200.0];
+    method = [[BrewMethod alloc] initWithName:@"Chemex" grind:grindTypes[1] coffeeAmount:44.0 waterAmount:700.0 temp:200.0];
     
     [self addBrewMethodWithBrewMethod:method];
     
     
-    method = [[BrewMethod alloc] initWithName:@"AeroPress" grind:grindTypes[3] coffeeAmount:28.0 waterAmount:490.0 temp:200.0];
+    method = [[BrewMethod alloc] initWithName:@"Hario V60 Dripper" grind:grindTypes[4] coffeeAmount:25.0 waterAmount:400.0 temp:210.0];
+    
+    [self addBrewMethodWithBrewMethod:method];
+    
+    method = [[BrewMethod alloc] initWithName:@"AeroPress" grind:grindTypes[2] coffeeAmount:17.0 waterAmount:100.0 temp:175.0];
+    
+    [self addBrewMethodWithBrewMethod:method];
+    method = [[BrewMethod alloc] initWithName:@"Melitta" grind:grindTypes[3] coffeeAmount:23.0 waterAmount:336.0 temp:205.0];
     
     [self addBrewMethodWithBrewMethod:method];
     
