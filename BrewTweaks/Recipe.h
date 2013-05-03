cc2cc24696413a273ba415f720a368955b8e89a3
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-05-03 13:43:41 -0400

    Added a tabbed view

---
+++ BrewTweaks/Recipe.h
@@ -0,0 +1,13 @@
+//
+//  Recipe.h
+//  BrewTweaks
+//
+//  Created by Pamela Ocampo on 5/3/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recipe : NSObject
+
+@end
---
+++ BrewTweaks/Recipe.m
@@ -0,0 +1,13 @@
+//
+//  Recipe.m
+//  BrewTweaks
+//
+//  Created by Pamela Ocampo on 5/3/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import "Recipe.h"
+
+@implementation Recipe
+
+@end
